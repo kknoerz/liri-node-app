b242f81141126fd1b4c5b902589ebab2f1e5449e
kknoerz <kevink430@gmail.com>
2016-04-09 12:52:12 -0400

    spotify working

--- liri.js
+++ liri.js
@@ -1,8 +1,10 @@
 var fs = require('fs');
+var keys = require('./keys.js');
+
+//*******************************TWITTER****************************************
 
 if (process.argv[2]==="my-tweets"){
   var Twitter = require('twitter');
-  var keys = require('./keys.js')
 
     var client = new Twitter({
       consumer_key: keys.twitterKeys.consumer_key,
@@ -24,7 +26,28 @@ if (process.argv[2]==="my-tweets"){
     });
 }
 
+//******************************* SPOTIFY **************************************
+
 if (process.argv[2]==="spotify-this-song"){
+  var spotify = require('spotify');
+
+  spotify.search({ type: 'track', query: process.argv[3] }, function(err, data) {
+      if ( err ) {
+          console.log('Error occurred: ' + err);
+          return;
+      }
+      debugger;
+      // console.log(data.tracks)
+      for (j=0;j<data.tracks.items.length;j++){
+      console.log('Artist name: ', data.tracks.items[j].artists[0].name)
+      console.log('Track name: ', data.tracks.items[j].name)
+      console.log('Link to the song: ', data.tracks.items[j].href)
+      console.log('Album name: ', data.tracks.items[j].album.name)
+      console.log('*******************************************************')
+      console.log(' ')
+    }
+
+  });
 
 }
 
