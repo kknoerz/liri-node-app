a81379741940c22a029ff49d77bb4e756937862a
kknoerz <kevink430@gmail.com>
2016-04-11 16:28:10 -0400

    cleaned up console.log spotify

--- liri.js
+++ liri.js
@@ -21,7 +21,10 @@ if (process.argv[2]==="my-tweets"){
         console.log(error)
       }
       for (i=0;i<tweets.length;i++){
+        console.log(' ')
         console.log(tweets[i].text)
+        console.log('*******************************************************')
+        console.log(' ')
       }
     });
 }
@@ -31,27 +34,40 @@ if (process.argv[2]==="my-tweets"){
 if (process.argv[2]==="spotify-this-song"){
   var spotify = require('spotify');
 
-  spotify.search({ type: 'track', query: process.argv[3] }, function(err, data) {
-      if ( err ) {
-          console.log('Error occurred: ' + err);
-          return;
-      }
-      debugger;
-      // console.log(data.tracks)
-      for (j=0;j<data.tracks.items.length;j++){
-      console.log('Artist name: ', data.tracks.items[j].artists[0].name)
-      console.log('Track name: ', data.tracks.items[j].name)
-      console.log('Link to the song: ', data.tracks.items[j].href)
-      console.log('Album name: ', data.tracks.items[j].album.name)
-      console.log('*******************************************************')
-      console.log(' ')
+  spotify.search({ type: 'track', query: process.argv[3] }, function(error, data) {
+    if (data.tracks.items.length == 0){
+      var path = "https://api.spotify.com/v1/artists/6FBDaR13swtiWwGhX1WQsP"
+      spotify.search(path, function(error, data) {
+        debugger;
+        console.log(data)
+
+      });
     }
+    if (error) {
+        console.log('Error occurred: ' + error);
+        return;
+    }
+    debugger;
+    // console.log(data.tracks)
+    for (j=0;j<data.tracks.items.length;j++){
+    var num = j+1;
+    console.log(' ')
+    console.log('Result #'+num)
+    console.log('Artist name: ', data.tracks.items[j].artists[0].name)
+    console.log('Track name: ', data.tracks.items[j].name)
+    console.log('Link to the song: ', data.tracks.items[j].href)
+    console.log('Album name: ', data.tracks.items[j].album.name)
+    console.log('**************************************************************************************************************')
+    console.log(' ')
+    }
+
 
   });
 
 }
 
 if (process.argv[2]==="movie-this"){
+  
 
 }
 
