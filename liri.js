6e1431b695961fe00071e7d3ff59f5449846bbd3
kknoerz <kevink430@gmail.com>
2016-04-11 18:32:23 -0400

    movie-this working

--- liri.js
+++ liri.js
@@ -14,7 +14,7 @@ if (process.argv[2]==="my-tweets"){
     });
 
     var path  = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
-    var params = 'KnoerzerKevin'
+    var params = 'KnoerzerKevin';
 
     client.get(path, params, function(error, tweets, response){
       if (error){
@@ -27,7 +27,7 @@ if (process.argv[2]==="my-tweets"){
         console.log(' ')
       }
     });
-}
+};
 
 //******************************* SPOTIFY **************************************
 
@@ -38,8 +38,7 @@ if (process.argv[2]==="spotify-this-song"){
     if (error) {
         console.log('Error occurred: ' + error);
         return;
-    }
-    debugger;
+    };
     if (data.tracks.items.length == 0){
       var path = "https://api.spotify.com/v1/tracks/7GhIk7Il098yCjg4BQjzvb"
       spotify.get(path, function(error, data) {
@@ -51,30 +50,67 @@ if (process.argv[2]==="spotify-this-song"){
         console.log(rick)
 
       });
-    }
+    };
     // console.log(data.tracks)
     for (j=0;j<data.tracks.items.length;j++){
-    var num = j+1;
-    console.log(' ')
-    console.log('Result #'+num)
-    console.log('Artist name: ', data.tracks.items[j].artists[0].name)
-    console.log('Track name: ', data.tracks.items[j].name)
-    console.log('Link to the song: ', data.tracks.items[j].href)
-    console.log('Album name: ', data.tracks.items[j].album.name)
-    console.log('**************************************************************************************************************')
-    console.log(' ')
-    }
-
-
+      var num = j+1;
+      console.log(' ');
+      console.log('Result #'+num);
+      console.log('Artist name: ', data.tracks.items[j].artists[0].name);
+      console.log('Track name: ', data.tracks.items[j].name);
+      console.log('Link to the song: ', data.tracks.items[j].href);
+      console.log('Album name: ', data.tracks.items[j].album.name);
+      console.log('**************************************************************************************************************')
+      console.log(' ');
+    };
   });
 
-}
+};
 
 if (process.argv[2]==="movie-this"){
+  var request = require('request')
+  var query = process.argv[3];
+  var search = 'http://www.omdbapi.com/?t='+query+'&y=&plot=short&r=json&tomatoes=true';
+  request.get(search, function(error, data, body){
 
+    jsonBody = JSON.parse(body);
 
-}
+    if(jsonBody.Error){
+      var nobody = "Mr. Nobody";
+      search = 'http://www.omdbapi.com/?t='+nobody+'&y=&plot=short&r=json&tomatoes=true';
+      request.get(search, function(error, data, body){
+        jsonBody = JSON.parse(body);
+        console.log(' ');
+        console.log("YOUR SEARCH "+process.argv[3]+" GOT NO RESPONSE, BUT YOU SHOULD CHECK OUT THIS MOVIE INSTEAD ABOUT IMMORTALITY AND CHOICES. IT'S VERY NUANCED AND IT HAS JARED LETO, HE'S PRETTY COOL");
+        console.log('The title is: ', jsonBody.Title);
+        console.log('The year it came out: ', jsonBody.Year);
+        console.log('IMDB Rating: ', jsonBody.imdbRating);
+        console.log('Released country: ', jsonBody.Country);
+        console.log('Language: ', jsonBody.Language);
+        console.log('Plot: ', jsonBody.Plot);
+        console.log('Actors: ', jsonBody.Actors);
+        console.log('Tomatoe Rating: ', jsonBody.tomatoMeter);
+        console.log('Tomatoe URL: ', jsonBody.tomatoURL);
+        console.log('**************************************************************************************************************');
+        console.log(' ');
+      });
+    }else{
+      console.log(' ');
+      console.log('The title is: ', jsonBody.Title);
+      console.log('The year it came out: ', jsonBody.Year);
+      console.log('IMDB Rating: ', jsonBody.imdbRating);
+      console.log('Released country: ', jsonBody.Country);
+      console.log('Language: ', jsonBody.Language);
+      console.log('Plot: ', jsonBody.Plot);
+      console.log('Actors: ', jsonBody.Actors);
+      console.log('Tomatoe Rating: ', jsonBody.tomatoMeter);
+      console.log('Tomatoe URL: ', jsonBody.tomatoURL);
+      console.log('**************************************************************************************************************');
+      console.log(' ');
+    };
+  });
+};
 
 if (process.argv[2]==="do-what-it-says"){
-
+  
 }
