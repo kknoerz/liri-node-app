9a86c64dd33cf2bfbaedb77609a5c3c6b50cbaea
kknoerz <kevink430@gmail.com>
2016-04-12 11:13:35 -0400

    finished do what it says

--- liri.js
+++ liri.js
@@ -2,57 +2,59 @@ var fs = require('fs');
 var keys = require('./keys.js');
 
 //*******************************TWITTER****************************************
-
-if (process.argv[2]==="my-tweets"){
+var runTwitter = function(){
   var Twitter = require('twitter');
 
-    var client = new Twitter({
-      consumer_key: keys.twitterKeys.consumer_key,
-      consumer_secret: keys.twitterKeys.consumer_secret,
-      access_token_key: keys.twitterKeys.access_token_key,
-      access_token_secret: keys.twitterKeys.access_token_secret
-    });
+  var client = new Twitter({
+    consumer_key: keys.twitterKeys.consumer_key,
+    consumer_secret: keys.twitterKeys.consumer_secret,
+    access_token_key: keys.twitterKeys.access_token_key,
+    access_token_secret: keys.twitterKeys.access_token_secret
+  });
 
-    var path  = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
-    var params = 'KnoerzerKevin';
+  var path  = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
+  var params = 'KnoerzerKevin';
 
-    client.get(path, params, function(error, tweets, response){
-      if (error){
-        console.log(error)
-      }
-      for (i=0;i<tweets.length;i++){
-        console.log(' ')
-        console.log(tweets[i].text)
-        console.log('*******************************************************')
-        console.log(' ')
-      }
-    });
+  client.get(path, params, function(error, tweets, response){
+    if (error){
+      console.log(error);
+    }
+    for (var i=0;i<tweets.length;i++){
+      console.log(' ');
+      console.log(tweets[i].text);
+      console.log('*******************************************************');
+      console.log(' ');
+    }
+  });
 };
 
-//******************************* SPOTIFY **************************************
+if (process.argv[2]==="my-tweets"){
+  runTwitter();
+}
 
-if (process.argv[2]==="spotify-this-song"){
+//******************************* SPOTIFY **************************************
+var runSpotify = function(){
   var spotify = require('spotify');
 
   spotify.search({ type: 'track', query: process.argv[3] }, function(error, data) {
     if (error) {
         console.log('Error occurred: ' + error);
         return;
-    };
-    if (data.tracks.items.length == 0){
-      var path = "https://api.spotify.com/v1/tracks/7GhIk7Il098yCjg4BQjzvb"
+    }
+    if (data.tracks.items.length === 0){
+      var path = "https://api.spotify.com/v1/tracks/7GhIk7Il098yCjg4BQjzvb";
       spotify.get(path, function(error, data) {
-        var rick = require('./rickRoll.js')
-        console.log(' ')
-        console.log('Artist name: ', data.artists[0].name)
-        console.log('Track name: ', data.name)
+        var rick = require('./rickRoll.js');
+        console.log(' ');
+        console.log('Artist name: ', data.artists[0].name);
+        console.log('Track name: ', data.name);
         console.log("Rick Roll'd try again!");
-        console.log(rick)
+        console.log(rick);
 
       });
-    };
+    }
     // console.log(data.tracks)
-    for (j=0;j<data.tracks.items.length;j++){
+    for (var j=0;j<data.tracks.items.length;j++){
       var num = j+1;
       console.log(' ');
       console.log('Result #'+num);
@@ -60,15 +62,18 @@ if (process.argv[2]==="spotify-this-song"){
       console.log('Track name: ', data.tracks.items[j].name);
       console.log('Link to the song: ', data.tracks.items[j].href);
       console.log('Album name: ', data.tracks.items[j].album.name);
-      console.log('**************************************************************************************************************')
+      console.log('**************************************************************************************************************');
       console.log(' ');
-    };
+    }
   });
-
 };
 
-if (process.argv[2]==="movie-this"){
-  var request = require('request')
+if (process.argv[2]==="spotify-this-song"){
+  runSpotify();
+}
+//******************************* MOVIE-THIS **************************************
+var runMovieThis = function(){
+  var request = require('request');
   var query = process.argv[3];
   var search = 'http://www.omdbapi.com/?t='+query+'&y=&plot=short&r=json&tomatoes=true';
   request.get(search, function(error, data, body){
@@ -107,19 +112,27 @@ if (process.argv[2]==="movie-this"){
       console.log('Tomatoe URL: ', jsonBody.tomatoURL);
       console.log('**************************************************************************************************************');
       console.log(' ');
-    };
+    }
   });
 };
 
-if (process.argv[2]==="do-what-it-says"){
-  debugger;
+if (process.argv[2]==="movie-this"){
+  runMovieThis();
+}
+
+//******************************* DO-WHAT-IT-SAYS **************************************
+var runDoWhatItSays = function() {
   var fs = require('fs');
   var readIt = './random.txt';
   var random = fs.readFileSync(readIt, 'utf8');
-  console.log(typeof(random));
-  var spotify = random.slice(0,17)
-  var song = random.slice(18,38)
-  console.log(random)
-  console.log(spotify)
-  console.log(song)
+  var spotify = random.slice(0,17);
+  var song = random.slice(18,38);
+
+  process.argv[2] = spotify;
+  process.argv[3] = song;
 };
+
+if (process.argv[2]==="do-what-it-says"){
+  runDoWhatItSays();
+  runSpotify();
+}
